e2d6195cb7deca16cbb0be51176b7b1f711fdd61
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-05-22 19:30:23 -0300

    Felipe 02.py

---
+++ Felipe 02.py
@@ -0,0 +1,11 @@
+# 2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+nome = input("Digite seu nome: ")
+estado = input("Digite o estado em que você mora: ")
+municipio = input("Digite seu município: ")
+
+
+print("Seu nome é " + nome)
+print("Você mora no estado do " + estado)
+print("Seu município é " + municipio)
+
