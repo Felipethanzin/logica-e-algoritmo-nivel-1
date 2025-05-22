3b288a04fc227fd07639ddd59cc1d7ab05e9448b
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-05-22 19:34:19 -0300

    Felipe 06.py

---
+++ Felipe 06.py
@@ -0,0 +1,17 @@
+#6. Menu - Elabore um programa que mostre o seguinte menu na tela:
+Cadastro de Clientes
+0 - Fim
+1 - Inclui
+2 - Altera
+3 - Exclui
+4 - Consulta
+
+Opção: 
+
+print("0-Fim")
+print("1-Inclui")
+print("2-Alterar")
+print("3-Excluir")
+print("4-Consultar")
+input("")
+
