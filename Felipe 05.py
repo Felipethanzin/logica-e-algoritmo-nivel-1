0fbf81eca3554dc4bb9033d5fcf7b71a57c7d40f
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-05-22 19:33:22 -0300

    Felipe 05.py

---
+++ Felipe 05.py
@@ -0,0 +1,16 @@
+#5. Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+
+ALUNO(A)          NOTA
+=========         =====
+ALINE              9.0  
+MÁRIO              DEZ
+SÉRGIO             4.5    
+SHIRLEY            7.0
+
+print("Aluno(a)         Nota")
+print("=======          =====")
+print("ALINE            9.0")
+print("MARIO            DEZ")
+print("SERGIO           4.5")
+print("SHIRLEY          7.0")
+
