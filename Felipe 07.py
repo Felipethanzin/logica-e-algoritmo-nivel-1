1de03ed9be895e2d691158774b1f2cd34ad4817d
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-05-22 19:34:51 -0300

    Felipe 07.py

---
+++ Felipe 07.py
@@ -0,0 +1,13 @@
+ # 7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+ Enriqueça o desenho com outros caracteres, simulando enfeites.
+       X
+      XXX
+     XXXXX
+    XXXXXXX
+   XXXXXXXXX
+  XXXXXXXXXXX
+ XXXXXXXXXXXXX
+XXXXXXXXXXXXXXX
+       XX
+       XX
+      XXXX
