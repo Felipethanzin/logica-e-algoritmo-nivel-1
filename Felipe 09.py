8cbbd8d79f90d1dd0e3171aa342fa045b1fd7d45
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-05-22 21:19:26 -0300

    Felipe 09.py

---
+++ Felipe 09.py
@@ -0,0 +1,11 @@
+#9 - escreva um software que some dois números.
+
+
+print("Digite um número que quer somar")
+num1 = int(input())
+
+print("Digite outro número que quer somar")
+num2 = int(input())
+
+soma = num1 + num2
+print("A soma dos dois números é:", soma)
