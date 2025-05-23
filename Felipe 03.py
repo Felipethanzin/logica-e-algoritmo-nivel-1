7bee430defab92567bae051495ab12e10e5b3433
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-05-22 21:05:03 -0300

    Felipe 03.py

---
+++ Felipe 03.py
@@ -0,0 +1,6 @@
+#3. Letra de música - Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber).
+
+print("Qual é o nome da música que você gosta?")
+musica = input()
+
+print("Você gosta da música:", musica)
